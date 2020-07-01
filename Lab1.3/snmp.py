2383f084c1bd0cc8029b6f68ae855b770d179067
etezov <etezov@jet.su>
2020-07-01 18:35:21 +0300

    1

--- Lab1.3/snmp.py
+++ Lab1.3/snmp.py
@@ -10,16 +10,16 @@ result = getCmd(SnmpEngine(),
                 ContextData(),
                 ObjectType(snmp_object))
 
-for i in result:
-    for j in i[3]:
-        print(j)
-
 result2 = nextCmd(SnmpEngine(),
                   CommunityData('public', mpModel=0),
                   UdpTransportTarget(('10.31.70.107', 161)),
                   ContextData(),
                   ObjectType(snmp_object2),
                   lexicographicMode=False)
+
+for i in result:
+    for j in i[3]:
+        print(j)
 
 for i in result2:
     for j in i[3]:
