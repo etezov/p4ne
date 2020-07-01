2692851222b63b2fa942330e4dad665261fc01aa
etezov <etezov@jet.su>
2020-07-01 18:32:15 +0300

    1

---
+++ Lab1.3/snmp.py
@@ -0,0 +1,26 @@
+from pysnmp.hlapi import *
+
+
+snmp_object = ObjectIdentity('SNMPv2-MIB', 'sysDescr', 0)
+snmp_object2 = ObjectIdentity('1.3.6.1.2.1.2.2.1.2')
+
+result = getCmd(SnmpEngine(),
+                CommunityData('public', mpModel=0),
+                UdpTransportTarget(('10.31.70.107', 161)),
+                ContextData(),
+                ObjectType(snmp_object))
+
+for i in result:
+    for j in i[3]:
+        print(j)
+
+result2 = nextCmd(SnmpEngine(),
+                  CommunityData('public', mpModel=0),
+                  UdpTransportTarget(('10.31.70.107', 161)),
+                  ContextData(),
+                  ObjectType(snmp_object2),
+                  lexicographicMode=False)
+
+for i in result2:
+    for j in i[3]:
+        print(j)
