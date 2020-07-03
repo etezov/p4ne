76cd1bb8f0702ef5199c5256f919ecb81e820f14
etezov <etezov@jet.su>
2020-07-03 15:59:59 +0300

    1

--- Lab1.5/configfiles.py
+++ Lab1.5/configfiles.py
@@ -2,12 +2,12 @@ import glob
 
 dir = glob.glob("E:\работа\Python\p4ne_training\config_files\*.txt")
 
-iplist=[]
+iplist = []
 
 for i in dir:
     with open(i) as f:
         for l in f:
-            if (l.find("ip address")>=0) & (l.find(".")>=0):
+            if (l.find("ip address") >= 0) & (l.find(".") >= 0):
                 l = l.strip()
                 l = l.replace("ip address", "")
                 iplist.append(l)
